2c5bad2f3d23b3f6ade111416153f7526c81a0d0
achoma <139071702+achoma@users.noreply.github.com>
2024-12-17 14:10:03 +0100

    update

---
+++ js/contect-form.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const response = await fetch(form.action, {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  if (response.ok) {
+    alert("Your message has been sent successfully!");
+    form.reset();
+  } else {
+    alert(
+      "Oops! There was a problem sending your message. Please try again later."
+    );
+  }
+});
